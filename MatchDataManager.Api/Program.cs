77c23e1a3b0eb3006176ca50f9e919b23178e71d
Czes377813 <Czes377813@PL-ZRN-WL-U5178>
2023-07-10 15:37:48 +0200

    Add Seeder

--- MatchDataManager.Api/Program.cs
+++ MatchDataManager.Api/Program.cs
@@ -5,6 +5,7 @@ using MatchDataManager.Api.MiddleWare;
 using MatchDataManager.Api.Models;
 using MatchDataManager.Api.Models.Paination;
 using MatchDataManager.Api.Repositories;
+using MatchDataManager.Api.Seeder;
 using MatchDataManager.Api.Validators;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -25,7 +26,12 @@ builder.Services.AddScoped<ITeamInterface, TeamRepository>();
 builder.Services.AddScoped<ILocationInterface, LocationsServices>();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Services.AddScoped<IValidator<Query>,QueryValidator>();
+builder.Services.AddScoped<TeamSeeder>();
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<TeamSeeder>();
+seeder.Seed();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ MatchDataManager.Api/Seeder/TeamSeeder.cs
@@ -0,0 +1,60 @@
+﻿using MatchDataManager.Api.Models;
+
+namespace MatchDataManager.Api.Seeder
+{
+    public class TeamSeeder
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public TeamSeeder(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public void Seed()
+        {
+            if (_appDbContext.Database.CanConnect())
+            {
+                var team = GetTeam();
+                _appDbContext.Team.AddRange(team);
+                _appDbContext.SaveChanges();
+            }
+        }
+        private IEnumerable<Team> GetTeam()
+        {
+            var team = new List<Team>()
+            {
+                new Team()
+                {
+                    Name = "YellowBoys",
+                    CoachName = "Marcin"
+                },
+                 new Team()
+                 {
+                     Name = "BlueBoys",
+                     CoachName = "Andrzej"
+                 },
+                  new Team()
+                  {
+                      Name = "Greenboys",
+                      CoachName = "Adam"
+                  },
+                   new Team()
+                   {
+                       Name = "RedBoys",
+                       CoachName = "Adrian"
+                   },
+                    new Team()
+                    {
+                        Name = "GreyBoys",
+                        CoachName = "Paweł"
+                    },
+                     new Team()
+                     {
+                         Name = "ImgBoys",
+                         CoachName = "Krzysztof"
+                     }
+            };
+            return team;
+        }
+    }
+}
