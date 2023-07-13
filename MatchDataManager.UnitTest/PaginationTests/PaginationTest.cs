4f705135aa0749cd1968bff10fe39439fad4e547
Czes377813 <Czes377813@PL-ZRN-WL-U5178>
2023-07-13 15:34:43 +0200

    PaginationTest

---
+++ MatchDataManager.UnitTest/PaginationTests/PaginationTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using MatchDataManager.Api.Models;
+using MatchDataManager.Api.Models.Paination;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchDataManager.UnitTest.Pagination
+{
+    public class PaginationTest
+    {
+        [Fact]
+        public void GetTeam_CorrectlyAppliesPagination()
+        {
+            // Arrange
+            var teams = new List<Team>
+        {
+            new Team { Name = "Team1" },
+            new Team { Name = "Team2" },
+            new Team { Name = "Team3" },
+            new Team { Name = "Team4" },
+            new Team { Name = "Team5" },
+              new Team { Name = "Team1" },
+            new Team { Name = "Team2" },
+            new Team { Name = "Team3" },
+                  new Team { Name = "Team1" },
+            new Team { Name = "Team2" },
+            new Team { Name = "Team3" }
+
+        }.AsQueryable();
+
+            var query = new Query { PageSize = 2, PageNumber = 3};
+
+            // Act
+            var result = GetTeam(teams, query);
+
+            // Assert
+            result.Should().HaveCount(query.PageSize);
+            result.Should().Contain(teams.Skip(query.PageSize * (query.PageNumber - 1)).Take(query.PageSize));
+        }
+      
+
+        private List<Team> GetTeam(IQueryable<Team> teams, Query query)
+        {
+            return teams
+                .Skip(query.PageSize * (query.PageNumber - 1))
+                .Take(query.PageSize)
+                .ToList();
+        }
+
+      
+   
+   
+    }
+}
